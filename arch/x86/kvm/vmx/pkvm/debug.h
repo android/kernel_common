b28d79d9a1c7ad187b7c5b358b2c4fef8042095c
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-03-17 14:39:24 +0800

    ANDROID: pkvm: x86: Get cpu id via smp_processor_id
    As pkvm hypervisor has defined the pcpu_hot and initialized the
    cpu_number, smp_processor_id is able to be used to get the cpu id.
    Switch to use this macro and remove pkvm's own implementation.
    
    Bug: 391539939
    Test: pKVM works
    Upstream-Task: 402758258
    
    Change-Id: I44cf9cfa79e0847343065e645b82b0cdd9c77f92
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/vmx/pkvm/debug.h
+++ arch/x86/kvm/vmx/pkvm/debug.h
@@ -9,7 +9,6 @@
 void pkvm_debug_sym(__dynamic_pr_debug)(struct _ddebug *descriptor, const char *fmt, ...);
 int pkvm_debug_sym(_printk)(const char *fmt, ...);
 noinstr struct cpu_entry_area *pkvm_debug_sym(get_cpu_entry_area)(int cpu);
-unsigned long pkvm_debug_sym(get_pcpu_id)(void);
 
 #endif
 
