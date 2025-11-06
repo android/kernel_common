BayLibre/android_kernel_common

ANDROID: pkvm: x86: stop relying on shadow pgt pool
With memcache, backed with pages supplied by host, there is no need to
use upfront preallocated shadow pgt pool capable of handling page table
creation for all possible mapping for all predefined VM number.
However there is still need to have per VM pool which is used for
page table root initialization which happens during vm creation, before
vCPU is initialized (which prevents using per-CPU memcache). Therefore
allocate and donate single page from pKVM-high to pKVM-low during VM
initialization and use it for per-VM page table root initialization.

Bug: 391541180
Test: Run various VMs on Brya and Qemu

Change-Id: Ia851c130616ab8dfd4502b92a390c74c0c53cd47
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
@@ -14,6 +14,9 @@
 #include "memory.h"
 #include <pkvm/vmx/vmx.h>
 
+DEFINE_PER_CPU(struct pkvm_vm *, __current_vm);
+#define current_vm (*this_cpu_ptr(&__current_vm))
+
 struct check_walk_data {
 	int 			nstate;
 	enum pkvm_page_state	*desired;
@@ -52,14 +55,16 @@ struct pkvm_mem_transition {
 	struct pkvm_mem_trans_desc	completer;
 };
 
-static void guest_pgt_lock(struct pkvm_pgtable *pgt)
+void guest_pgt_lock(struct pkvm_vm *vm)
 {
-	pkvm_spin_lock(&pgt_to_pkvm(pgt)->pgt_lock);
+	pkvm_spin_lock(&vm->pgt_lock);
+	current_vm = vm;
 }
 
-static void guest_pgt_unlock(struct pkvm_pgtable *pgt)
+void guest_pgt_unlock(struct pkvm_vm *vm)
 {
-	pkvm_spin_unlock(&pgt_to_pkvm(pgt)->pgt_lock);
+	current_vm = NULL;
+	pkvm_spin_unlock(&vm->pgt_lock);
 }
 
 static u64 pkvm_init_invalid_leaf_owner(pkvm_id owner_id)
@@ -726,6 +731,7 @@ static int __pkvm_guest_share_host_page(struct pkvm_pgtable *guest_pgt,
 int __pkvm_guest_share_host(struct pkvm_pgtable *guest_pgt,
 			    u64 gpa, u64 size, void *mc)
 {
+	struct pkvm_vm *pkvm_vm = pgt_to_pkvm(guest_pgt);
 	unsigned long hpa;
 	u64 prot;
 	int ret = 0;
@@ -733,7 +739,7 @@ int __pkvm_guest_share_host(struct pkvm_pgtable *guest_pgt,
 	if (!PAGE_ALIGNED(size))
 		return -EINVAL;
 
-	guest_pgt_lock(guest_pgt);
+	guest_pgt_lock(pkvm_vm);
 	host_ept_lock();
 
 	while (size) {
@@ -752,7 +758,7 @@ int __pkvm_guest_share_host(struct pkvm_pgtable *guest_pgt,
 	}
 
 	host_ept_unlock();
-	guest_pgt_unlock(guest_pgt);
+	guest_pgt_unlock(pkvm_vm);
 
 
 	return ret;
@@ -975,11 +981,12 @@ static int __pkvm_guest_unshare_host_page(struct pkvm_pgtable *guest_pgt,
 int __pkvm_guest_unshare_host(struct pkvm_pgtable *guest_pgt,
 			      u64 gpa, u64 size)
 {
+	struct pkvm_vm *pkvm_vm = pgt_to_pkvm(guest_pgt);
 	unsigned long hpa;
 	u64 prot;
 	int ret = 0;
 
-	guest_pgt_lock(guest_pgt);
+	guest_pgt_lock(pkvm_vm);
 	host_ept_lock();
 
 	while (size) {
@@ -998,7 +1005,7 @@ int __pkvm_guest_unshare_host(struct pkvm_pgtable *guest_pgt,
 	}
 
 	host_ept_unlock();
-	guest_pgt_unlock(guest_pgt);
+	guest_pgt_unlock(pkvm_vm);
 
 	return ret;
 }

android/kernel_common

use an upfront preallocated shadow pgt pool capable of handling page
table creation for all possible mapping for all predefined VM numbers.
However there is still need to have per VM pool which is used for:
1) Root Page Table Initialization: During VM creation, the root page
   table for the guest's EPT must be initialized. This occurs before any
   vCPUs are initialized, making the per-vCPU memcache unavailable. A
   per-VM pool provides the initial page for the root.
2) Freed Page Placeholder: The per-VM pool serves as a temporary holding
   place for page table pages that are no longer in use. This can
   happen, for example, when all 512 PTEs within a page table page are
   unmapped (due to MMU notifiers e.g. with active ballooning) or when
   page-tables are being destroyed during VM teardown. The refcounting
   mechanism (hyp_get_page/hyp_put_page + guest_mmu_zalloc_page's
   hyp_set_page_refcounted) ensures these pages are tracked, and they
   can be added to the per-VM pool to be potentially reused by
   guest_mmu_zalloc_page or drained during VM teardown.
Therefore allocate and donate a single page from pKVM-high to pKVM-low
during VM initialization and use it for per-VM page table root
initialization.
Bug: 453743944
Test: Build, boot and run various VMs
Upstream-Task: 402758258

arch/x86/kvm/vmx/pkvm/hyp/mem_protect.c
@@ -14,6 +14,9 @@
 #include "memory.h"
 #include <pkvm/vmx/vmx.h>
 
+DEFINE_PER_CPU(struct pkvm_vm *, __current_vm);
+#define current_vm (*this_cpu_ptr(&__current_vm))
+
 struct check_walk_data {
 	int 			nstate;
 	enum pkvm_page_state	*desired;
@@ -52,14 +55,16 @@ struct pkvm_mem_transition {
 	struct pkvm_mem_trans_desc	completer;
 };
 
-static void guest_mmu_lock(struct pkvm_pgtable *pgt)
+void guest_mmu_lock(struct pkvm_vm *vm)
 {
-	pkvm_spin_lock(&pgt_to_pkvm(pgt)->mmu_lock);
+	pkvm_spin_lock(&vm->mmu_lock);
+	current_vm = vm;
 }
 
-static void guest_mmu_unlock(struct pkvm_pgtable *pgt)
+void guest_mmu_unlock(struct pkvm_vm *vm)
 {
-	pkvm_spin_unlock(&pgt_to_pkvm(pgt)->mmu_lock);
+	current_vm = NULL;
+	pkvm_spin_unlock(&vm->mmu_lock);
 }
 
 static u64 pkvm_init_invalid_leaf_owner(pkvm_id owner_id)
@@ -729,6 +734,7 @@ static int __pkvm_guest_share_host_page(struct pkvm_pgtable *guest_pgt,
 int __pkvm_guest_share_host(struct pkvm_pgtable *guest_pgt,
 			    u64 gpa, u64 size, struct pkvm_memcache *mc)
 {
+	struct pkvm_vm *pkvm_vm = pgt_to_pkvm(guest_pgt);
 	unsigned long hpa;
 	u64 prot;
 	int ret = 0;
@@ -736,7 +742,7 @@ int __pkvm_guest_share_host(struct pkvm_pgtable *guest_pgt,
 	if (!PAGE_ALIGNED(size))
 		return -EINVAL;
 
-	guest_mmu_lock(guest_pgt);
+	guest_mmu_lock(pkvm_vm);
 	host_ept_lock();
 
 	while (size) {
@@ -755,8 +761,7 @@ int __pkvm_guest_share_host(struct pkvm_pgtable *guest_pgt,
 	}
 
 	host_ept_unlock();
-	guest_mmu_unlock(guest_pgt);
-
+	guest_mmu_unlock(pkvm_vm);
 
 	return ret;
 }
@@ -978,11 +983,12 @@ static int __pkvm_guest_unshare_host_page(struct pkvm_pgtable *guest_pgt,
 int __pkvm_guest_unshare_host(struct pkvm_pgtable *guest_pgt,
 			      u64 gpa, u64 size)
 {
+	struct pkvm_vm *pkvm_vm = pgt_to_pkvm(guest_pgt);
 	unsigned long hpa;
 	u64 prot;
 	int ret = 0;
 
-	guest_mmu_lock(guest_pgt);
+	guest_mmu_lock(pkvm_vm);
 	host_ept_lock();
 
 	while (size) {
@@ -1001,7 +1007,7 @@ int __pkvm_guest_unshare_host(struct pkvm_pgtable *guest_pgt,
 	}
 
 	host_ept_unlock();
-	guest_mmu_unlock(guest_pgt);
+	guest_mmu_unlock(pkvm_vm);
 
 	return ret;
 }